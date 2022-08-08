b98ad22a610bedcce72e0098ab02d189866904aa
Mert Yeşilbaş <mert.yesilbas.888@hotmail.com>
2022-08-08 17:40:06 +0300

    Updated MainActivity for Android Permissions and Location

--- app/src/main/java/com/example/btkgooglemapskotlin/MapsActivity.kt
+++ app/src/main/java/com/example/btkgooglemapskotlin/MapsActivity.kt
@@ -1,8 +1,17 @@
 package com.example.btkgooglemapskotlin
 
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -16,6 +25,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,13 +41,61 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
 
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Latitude : Enlem // Longitude: Boylam
         // 50. yıl parkı 39.923749266541755, 32.87619097187372
+        /*
         val park = LatLng(39.923749266541755, 32.87619097187372)
         mMap.addMarker(MarkerOptions().position(park).title("XYZ"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(park,15f))
+        */
+        // Casting -> as ile yapılır.
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationListener = object: LocationListener{
+            override fun onLocationChanged(p0: Location) {
+                // konum değişince yapılacak işlemler
+                mMap.clear()
+                val guncelKonum = LatLng(p0.latitude,p0.longitude)
+                mMap.addMarker(MarkerOptions().position(guncelKonum).title("Güncel Konum"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(guncelKonum,15f))
+            }
+
+        }
+
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            // İzin verilmemiş
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),1)
+        }else{
+            // İzin verilmiş
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1,1f,locationListener)
+            val sonBilinenKonum = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (sonBilinenKonum != null){
+                val sonBilinenLatLng = LatLng(sonBilinenKonum.latitude,sonBilinenKonum.longitude)
+                mMap.addMarker(MarkerOptions().position(sonBilinenLatLng).title("Son Bilinen Konum"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sonBilinenLatLng,15f))
+
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1){
+            if(grantResults.size>0){
+                if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    // İzin verildi
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1,1f,locationListener)
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
